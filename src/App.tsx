956ece37d5a6a25ce08e71137822dd2197726d4c
shologin <mockingbird@mail.com>
2023-11-26 20:53:40 +0200

    fix: removed gh-pages and its scripts, added version stamp

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { ForecastBadgesContainer } from "./components/ForecastBadgesContainer";
 import { ForecastOptions } from "./components/ForecastOptions";
 import { Search } from "./components/Search";
 import { getWeather } from "./functions/getWeather";
-// mt-52
 
 function App() {
   const [searchOpen, setSearchOpen] = useState(false);
@@ -41,19 +40,13 @@ function App() {
     setForecastOption(val);
   };
 
-
-  console.log(weatherState);
   useEffect(() => {
     getWeather(locationState, setWeatherState);
-  }, [locationState]);
+  }, [locationState]);  
 
-  // useEffect(() => {
-  //   console.log(weatherState);
-  // }, [weatherState]);
   return (
     <div className="app background flex flex-col justify-center items-center py-10 my-5 xs:my-20 mx-auto rounded-md">
       <Toaster />
-      <p>V. 1.4</p>
       <div className="flex-center flex-col gap-5 xs:flex-row">
         <BadgePrimary
           searchOpen={searchOpen}
@@ -67,14 +60,11 @@ function App() {
         <Search locationState={locationState} setLocationState={setLocationState} handleOpenForecast={handleOpenForecast} />
       )}
       {forecastOpen && !searchOpen && <ForecastBadgesContainer weatherState={weatherState} forecastOption={forecastOption} />}
+      <div className="hidden xs:block absolute bottom-1 right-2">
+        <p className="text-xs">v1.5</p>
+      </div>
     </div>
   );
 }
 
 export default App;
-
-// fetch("http://api.weatherapi.com/v1/forecast.json?key=a6a87d8563134d0bace175318232111&days=3&q=id:2506755")
-//   .then((res) => {
-//     return res.json();
-//   })
-//   .then((data) => console.log(data));
